3aeae962382b9a335963cc1708e505dd4fe5dc00
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-03 15:05:53 +0200

    add action function and enqueue scripts

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 function add_styles()
 {
 	wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
-	wp_enqueue_style('font-awesome', get_tmeplate_directory_uri() . '/assets/css/fontawesome.min.css');
+	wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/fontawesome.min.css');
 }
 
 /**
@@ -21,3 +21,10 @@ function add_scripts()
 	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), false, true);
 	wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', array(), false, true);
 }
+
+/**
+ * Add actions
+ * add_action()
+ */
+add_action('wp_enqueue_scripts', 'add_styles');
+add_action('wp_enqueue_scripts', 'add_scripts');
