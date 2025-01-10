aeaf4b909a89c64887a87af453272acd8eadd2c6
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-01-10 16:49:25 +0200

    Comments

--- comments.php
+++ comments.php
@@ -1,7 +1,20 @@
 <?php
-
-if (comments_open()) {
-	wp_list_comments();
+	if (comments_open()) { ?>
+	 <h3 class="comments-count"><?php comments_number() ?></h3>
+<?php
+	echo '<ul class="list-unstyled comments-list">';
+	$comments_arguments = [
+		'max_depth' => 3,
+		'type' => 'comment'
+	];
+	
+	wp_list_comments($comments_arguments);
+	echo '<ul>';
+	
+	echo '<hr class="comment-separator"/>';
+	
+	comment_form();
 } else {
 	echo 'Comments are disabled';
 }
+?>
